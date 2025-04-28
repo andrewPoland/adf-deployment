87ec67e1498ab10f5e5dd83b7aa6f1aa0b719860
Andrew Poland <andrewpo.software@gmail.com>
2025-04-29 10:57:47 +1200

    fix: update wait script and bump test time

--- .deployment/scripts/WaitForRunningPipelinesToComplete.ps1
+++ .deployment/scripts/WaitForRunningPipelinesToComplete.ps1
@@ -6,15 +6,15 @@ param(
 
 # Start time (how far back to look)
 $startTime = (Get-Date).AddDays(-$LookbackDays).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+$endTime = (Get-Date).AddHours(1).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
 
 do {
-    $currentTime = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
 
     $runningPipelines = az datafactory pipeline-run query-by-factory `
         --resource-group $ResourceGroupName `
         --factory-name $DataFactoryName `
         --last-updated-after $startTime `
-        --last-updated-before $currentTime `
+        --last-updated-before $endTime `
         --filters operand="Status" operator="Equals" values="InProgress" `
         --query "value[].{PipelineName:pipelineName, RunId:runId}" `
         --output json | ConvertFrom-Json
